6e9fe8955cbccf7fb1d3a0aa666d4afa108fb60f
nrenzoni <31897391+nrenzoni@users.noreply.github.com>
2026-05-28 17:47:20 -0400

    suppress notebook output from setup_notebook_html

--- notebooks/common/display.py
+++ notebooks/common/display.py
@@ -7,8 +7,8 @@ from itables import init_notebook_mode
 
 def setup_notebook_html() -> None:
     """Configure notebook HTML rendering for plots and tabular outputs."""
-    LetsPlot.setup_html()
-    init_notebook_mode(all_interactive=True, connected=True)
+    _ = LetsPlot.setup_html()
+    _ = init_notebook_mode(all_interactive=True, connected=True)
 
 
 def setup_polars_display() -> None:
